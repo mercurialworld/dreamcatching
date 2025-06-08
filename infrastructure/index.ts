7dc99f15dddd7d49fedad9bac6b987d82809f41b
ポッチャマ <mercworld@pocha.moe>
2025-06-07 20:49:41 -0400

    ok this time

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -2,6 +2,6 @@ import * as cdk from "aws-cdk-lib";
 import { PochaMoeStack } from "./stack";
 
 const app = new cdk.App();
-new PochaMoeStack(app, "MafuyuStack", {
+new PochaMoeStack(app, "PochaMoeStack", {
     env: { account: "575108959833", region: "us-east-1" },
 });
